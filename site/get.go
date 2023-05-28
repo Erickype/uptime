624a0770447d5d19cda33023622c262e15d8ce40
Erickype <erickype@hotmail.com>
2023-05-28 14:09:03 -0500

    Site service Get endpoint

--- site/get.go
+++ site/get.go
@@ -1,7 +1,20 @@
 package site
 
+import "context"
+
 // Site is the struct to be monitored
 type Site struct {
 	ID  int    `json:"id,omitempty"`
 	URL string `json:"url,omitempty"`
+}
+
+// Get gets a Site by its ID.
+//
+//encore:api public method=GET path=/site/:siteID
+func (s *Service) Get(_ context.Context, siteID int) (*Site, error) {
+	var site Site
+	if err := s.db.Where("id = $1", siteID).First(&site).Error; err != nil {
+		return nil, err
+	}
+	return &site, nil
 }

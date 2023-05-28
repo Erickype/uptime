9e25ad366d7a1e258c05bf25f862a1749f7b2ed5
Erickype <erickype@hotmail.com>
2023-05-28 13:07:03 -0500

    Site service Add endpoint

---
+++ site/add.go
@@ -0,0 +1,21 @@
+package site
+
+import "context"
+
+// AddParams is the struct to add a Site
+type AddParams struct {
+	// URL is the url of the Site.If it doesn't contain a scheme
+	// (like "http:" or "https:") it defaults to "https:"."
+	URL string
+}
+
+// Add adds a new Site to the list of monitored sites
+//
+//encore:api public method=POST path=/site
+func (s *Service) Add(_ context.Context, params *AddParams) (*Site, error) {
+	site := &Site{URL: params.URL}
+	if err := s.db.Create(site).Error; err != nil {
+		return nil, err
+	}
+	return site, nil
+}
